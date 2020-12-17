aab9d576855d550e5e64781413c4953f5d5e802b
Pabolo Pires <pabolo18@gmail.com>
2020-12-17 15:43:05 -0300

    Create PromonstraoMVC

---
+++ ProMonstraoMVC/README.md
@@ -0,0 +1,9 @@
+# ProMonstraoMVC
+
+Implementação do projeto ProMonstrao usando Spring MVC
+
+## Instruções
+
+1. Executar o script `db/MySQL/create.sql`, para inicializar a base de dados MySQL.
+
+2. Executar o comando `mvn spring-boot:run`. O sistema estará rodando em http://localhost:8080.
---
+++ ProMonstraoMVC/src/main/java/br/ufscar/dc/dsw/promonstraomvc/ProMonstraoMvcApplication.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw.promonstraomvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProMonstraoMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProMonstraoMvcApplication.class, args);
+    }
+
+}
